5a089a05fb90857a24e744aecaff30e7ff361ce3
Vheos <Vheos777@gmail.com>
2022-01-12 06:07:37 +0100

    added enums `SceneOperation` and `SceneOperationTiming`

--- Package/Code/Runtime/Managers/Enums.cs
+++ Package/Code/Runtime/Managers/Enums.cs
@@ -3,15 +3,6 @@ namespace Vheos.Tools.UnityCore
     using System;
     using UnityEngine;
 
-    // Defines
-    [Serializable]
-    public class SceneOperationOrder
-    {
-        public SceneOperation FirstOperation = SceneOperation.Unload;
-        public SceneOperationTiming FirstOperationTiming = SceneOperationTiming.Synchronously;
-        public SceneOperation SecondOperation = SceneOperation.Load;
-        public SceneOperationTiming SecondOperationTiming = SceneOperationTiming.Synchronously;
-    }
     public enum SceneOperation
     {
         Load,
