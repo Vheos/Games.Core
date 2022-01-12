59ca8b4a422410cbe13fb4d99c9c1516a54ca2a5
Vheos <Vheos777@gmail.com>
2022-01-12 05:54:44 +0100

    added attribute `ScenePicker` and its property drawer

---
+++ Package/Code/Editor/ScenePickerAttribute_Property_Drawer.cs
@@ -0,0 +1,25 @@
+#if UNITY_EDITOR
+namespace Vheos.Tools.UnityCore.Editor
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(ScenePickerAttribute))]
+    public class ScenePickerAttribute_PropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.String)
+                return;
+
+            var previousSceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(property.stringValue);
+            var currentSceneAsset = EditorGUI.ObjectField(rect, label, previousSceneAsset, typeof(SceneAsset), true);
+
+            if (currentSceneAsset == null)
+                property.stringValue = "";
+            else if (currentSceneAsset != previousSceneAsset)
+                property.stringValue = AssetDatabase.GetAssetPath(currentSceneAsset);
+        }
+    }
+}
+#endif
---
+++ Package/Code/Runtime/Attributes/ScenePickerAttribute.cs
@@ -0,0 +1,9 @@
+namespace Vheos.Tools.UnityCore
+{
+    using System;
+    using UnityEngine;
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class ScenePickerAttribute : PropertyAttribute
+    { }
+}
