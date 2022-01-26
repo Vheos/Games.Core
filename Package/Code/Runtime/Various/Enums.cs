53ea101c1fd249beb61fb3f14280b07fdb1a953f
Vheos <Vheos777@gmail.com>
2022-01-26 03:05:20 +0100

    added enum `BuiltInLayer`

---
+++ Package/Code/Runtime/Various/Enums.cs
@@ -0,0 +1,11 @@
+namespace Vheos.Games.Core
+{
+    public enum BuiltInLayer
+    {
+        Default = 0,
+        TransparentFX = 1,
+        IgnoreRaycast = 2,
+        Water = 4,
+        UI = 5,
+    }
+}
