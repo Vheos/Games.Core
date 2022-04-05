79f098edaca54fabce48f198a920f54895280f4b
Vheos <Vheos777@gmail.com>
2022-04-05 20:38:08 +0200

    added `Axes` flag combinations

--- Package/Code/Runtime/Various/Enums.cs
+++ Package/Code/Runtime/Various/Enums.cs
@@ -1,6 +1,8 @@
 namespace Vheos.Games.Core
 {
     using System;
+    using UnityEngine;
+    using Vheos.Tools.Extensions.General;
 
     public enum BuiltInLayer
     {
@@ -11,14 +13,6 @@ namespace Vheos.Games.Core
         UI = 5,
     }
 
-    [Flags]
-    public enum Axes
-    {
-        X = 1 << 0,
-        Y = 1 << 1,
-        Z = 1 << 2,
-    }
-
     public enum ExpandableState
     {
         Expanding,
@@ -27,11 +21,32 @@ namespace Vheos.Games.Core
         Collapsed,
     }
 
-
     public enum PredefinedTeam
     {
         None,
         Allies,
         Enemies,
+    }
+
+    [Flags]
+    public enum Axes
+    {
+        X = 1 << 0,
+        Y = 1 << 1,
+        Z = 1 << 2,
+
+        XY = X | Y,
+        XZ = X | Z,
+        YX = XY,
+        YZ = Y | Z,
+        ZX = XZ,
+        ZY = YZ,
+
+        XYZ = X | Y | Z,
+        XZY = XYZ,
+        YXZ = XYZ,
+        YZX = XYZ,
+        ZXY = XYZ,
+        ZYX = XYZ,
     }
 }
